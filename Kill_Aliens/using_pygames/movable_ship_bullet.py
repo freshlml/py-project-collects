9ba102ca1ba75c58f5c541fcdb8843c0a450a93f
freshlml <1326191992@qq.com>
2023-02-16 01:04:06 +0800

    kill aliens 样例

--- Kill_Aliens/using_pygames/movable_ship_bullet.py
+++ Kill_Aliens/using_pygames/movable_ship_bullet.py
@@ -1,10 +1,13 @@
 import sys
 import pygame
+import random
+
 
 pygame.init()
 size = width, height = 800, 1000
 speed = 1
 color = (100, 100, 100)
+finished = False
 
 __direction = {
     "NO": 0,
@@ -20,15 +23,56 @@ vertical_direction = __direction['NO']
 screen = pygame.display.set_mode(size)
 screen_rect = screen.get_rect()
 
-ship_image = pygame.image.load("zoom.svg")  # todo, pygame 对svg支持度不行
-# ship_image = pygame.image.load("airplane-40453_640.png")
-# ship_image = pygame.transform.smoothscale(ship_image, (100, 150))  # todo png 缩小后竟然变模糊
-ship_rect = ship_image.get_rect()
 
-ship_rect.centerx = screen_rect.centerx
-ship_rect.bottom = screen_rect.bottom
+class Ship(pygame.sprite.Sprite):
 
-bls = []
+    def __init__(self, screen_rect):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("zoom.svg")  # todo, pygame 对svg支持度不行
+        # ship_image = pygame.image.load("airplane-40453_640.png")
+        # ship_image = pygame.transform.smoothscale(ship_image, (100, 150))  # todo png 缩小后竟然变模糊
+        self.rect = self.image.get_rect()
+        self.rect.centerx = screen_rect.centerx
+        self.rect.bottom = screen_rect.bottom
+
+
+ship = Ship(screen_rect)
+ship_rect = ship.rect
+
+bls = pygame.sprite.Group()
+
+
+class Bullet(pygame.sprite.Sprite):
+
+    def __init__(self, screen, top, centerx):
+        pygame.sprite.Sprite.__init__(self)
+        self.screen = screen
+        self.rect = pygame.Rect(0, 0, 3, 15)
+        self.rect.top = top
+        self.rect.centerx = centerx
+
+
+aliens = pygame.sprite.Group()
+
+
+class Alien(pygame.sprite.Sprite):
+
+    def __init__(self, screen, ship_rect):
+        pygame.sprite.Sprite.__init__(self)
+        self.screen = screen
+        # self.rect = pygame.Rect(0, 0, 30, 30)
+        self.image = pygame.image.load("al.svg")
+        self.rect = self.image.get_rect()
+        ll = random.randint(ship_rect.width/2, screen.get_width() - ship_rect.width/2)
+        self.rect.left = ll
+
+
+def restart():
+    bls.empty()
+    aliens.empty()
+    ship_rect.centerx = screen_rect.centerx
+    ship_rect.bottom = screen_rect.bottom
+
 
 while True:
 
@@ -46,10 +90,16 @@ while True:
             if event.key == pygame.K_RIGHT:
                 horizontal_direction = __direction['RIGHT']
             if event.key == pygame.K_s:
-                bl = pygame.Rect(0, 0, 3, 15)
-                bl.top = ship_rect.top
-                bl.centerx = ship_rect.centerx
-                bls.append(bl)
+                bl = Bullet(screen, ship_rect.top, ship_rect.centerx)
+                bls.add(bl)
+            if event.key == pygame.K_a:
+                poi = -6
+                for i in range(3):
+                    bls.add(Bullet(screen, ship_rect.top, ship_rect.centerx + poi + -poi * i))
+            if event.key == pygame.K_SPACE:
+                if finished:
+                    restart()
+                    finished = False
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_UP and vertical_direction & __direction['UP']:
                 vertical_direction = __direction['NO']
@@ -80,16 +130,40 @@ while True:
         if ship_rect.bottom > height:
             ship_rect.bottom = height
 
+    if finished: continue
+
     for bl in bls:
-        bl.top -= 1
+        bl.rect.top -= 1
     for bl in bls:
-        if bl.bottom < 0:
+        if bl.rect.bottom <= 0:
             bls.remove(bl)
 
+    for al in aliens:
+        r = random.randint(0, 1)
+        al.rect.top += r
+
+        if al.rect.top < 0:
+            al.tect.top = 0
+        if al.rect.bottom > height:
+            aliens.remove(al)
+            del al
+        elif pygame.sprite.collide_mask(ship, al):
+            finished = True
+
+    # if pygame.sprite.spritecollideany(ship, aliens):
+    #     finished = True
+    collisions = pygame.sprite.groupcollide(bls, aliens, True, True)
+
+    if len(aliens) < 5:
+        for i in range(len(aliens), 5):
+            al = Alien(screen, ship_rect)
+            aliens.add(al)
+
     screen.fill(color)
-    screen.blit(ship_image, ship_rect)
+    screen.blit(ship.image, ship_rect)
     for bl in bls:
         pygame.draw.rect(screen, 255, bl)
+    for al in aliens:
+        pygame.draw.rect(screen, (0, 255, 0), al)
     pygame.display.flip()
-
 
