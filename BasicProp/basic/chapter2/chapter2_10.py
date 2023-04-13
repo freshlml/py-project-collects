a5c870f347230896e580d8697eaa68b1b84d747c
freshlml <1326191992@qq.com>
2023-04-14 00:13:10 +0800

    1

--- BasicProp/basic/chapter2/chapter2_10.py
+++ BasicProp/basic/chapter2/chapter2_10.py
@@ -5,7 +5,7 @@ from collections.abc import Iterator
 # list与array存储形式不同
 lst = [1, 2, 3, 4]                # lst指向class list的实例对象，实例对象空间中的数组，存class int实例对象的引用
 arr = array("l", (1, 2, 3, 4))    # arr指向class array的实例对象，实例对象空间的数组，存数值，模型等同于 java中primitive数组
-
+print(type(arr))  # <class 'ArrayType'>
 
 a_l = array("l", (1, 2147483647, -2147483648))  # 4个字节有符号整数数组，max=2^31-1=2147483647; min=-2^31=-2147483648
 print(a_l)
