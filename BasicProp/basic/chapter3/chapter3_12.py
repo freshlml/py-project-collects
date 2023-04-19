19a0ff0a2e3f4b60863de8091a7727d506749651
freshlml <1326191992@qq.com>
2023-04-20 01:02:39 +0800

    对象真值测试

--- BasicProp/basic/chapter3/chapter3_12.py
+++ BasicProp/basic/chapter3/chapter3_12.py
@@ -1,10 +1,38 @@
 
-# 真值测试
-# 非零数字和非空字符串为True，数字0和空字符串为False，None是False
-# list、tuple、dic、set递归比较 or relative with __bool__方法 todo
+# True and False literals
+print(type(True))  # <class 'bool'>, True被解析成 class bool 类型的实例对象
+print(type(False))  # <class 'bool'>, False被解析成 class bool 类型的实例对象
 
-print(type(True))  # <class 'bool'>, bool类型对象，存True(整数1)
-print(type(False))  # <class 'bool'>, bool类型对象，存False(整数0)
+# class bool is immutable
+a = True
+b = True
+print(a is b)  # True, 程序中所有的True均为同一个对象
+
+
+# 对象真值测试, __bool__协议方法, __len__协议方法
+#     if obj:  # 触发 __bool__ or __len__ 协议方法: return __bool__() or return __len__() > 0
+#
+# class int 类型 __bool__ 协议方法: return 整数值 != 0
+if 1:
+    print("整数值 != 0")
+
+# class float 类型 __bool__ 协议方法: return 浮点数 != 0.0
+if 0.1:
+    print("浮点数 != 0.0")
+
+# class str 类型 __len__ 协议方法
+if "some":
+    print("non-empty")
+
+# None is false
+if None:
+    print("None is true")
+else:
+    print("None is false")
+
+
+print("-------1-------")
+
 
 # 逻辑运算， and、or、not
 # a and b:  测试对象a，如果对象a为False返回对象a(短路)[注意不是返回False对象]
