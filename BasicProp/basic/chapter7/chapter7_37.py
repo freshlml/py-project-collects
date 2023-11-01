ced9a0a566cbcb15ffa34ad4fce22d261eeccc9d
freshlml <1326191992@qq.com>
2023-11-01 23:03:09 +0800

    1

--- BasicProp/basic/chapter7/chapter7_37.py
+++ BasicProp/basic/chapter7/chapter7_37.py
@@ -7,8 +7,8 @@ import chapter7_37_1
 
 config.fileConfig("log_config")
 logger = logging.getLogger('simpleExample')
-print(logger)
-print(logger is chapter7_37_1.logger)
+print(logger)                             # <Logger simpleExample (DEBUG)>
+print(logger is chapter7_37_1.logger)     # True
 logger.debug("chapter7_37运行")
 chapter7_37_1.md()
 chapter7_37.sub.md()
